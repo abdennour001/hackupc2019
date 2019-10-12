b5d52c34f5042692d50cc63e54e58af12901cd64
abdennour <amokranabdennour@gmail.com>
2019-10-12 03:20:33 +0200

    Adding home page.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid w-75 p-5">
+        <div class="row text-center">
+            <div class="col-12 mb-5">
+                <img style="width: 20%" src="https://miro.medium.com/max/6580/1*m7y8V4eoihnIcGm-ZL-z0A.png" alt="logo goes here...">
+            </div>
+            <div class="col-12 mb-5">
+                <h1 class="display-4">Our HackUPC 2019 nice App</h1>
+            </div>
+            <div class="col-12 mb-5">
+                <p class="lead" style="font-size: 1.3rem">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            </div>
+            <div class="col-12">
+                <div class="d-flex justify-content-center">
+                    <div class="link-container">
+                        <a href="#" class="custom-link"><i class="fa fa-rocket"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection

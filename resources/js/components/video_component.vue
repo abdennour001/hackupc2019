c9f1090dcbfecedc953d2b9831eea0475022988b
harshkaria <harsh.karia1@gmail.com>
2019-10-12 23:47:29 +0200

    Bla

--- resources/js/components/video_component.vue
+++ resources/js/components/video_component.vue
@@ -27,12 +27,13 @@ export default {
      previewTracks: '',
      identity: '',
      roomName: "",
-     username: "username", // Hard-coded: to-do: a form to capture this data
-     room_name: "demo"
+     username: "",
+     room_name: ""
    }
  },
- props: ['username', 'room_name'], // props that will be passed to this component
+ props: [], // props that will be passed to this component
  created() {
+      
       this.createChat(this.room_name);
     
 
@@ -42,7 +43,7 @@ export default {
  },
   methods: {
     async getAccessToken() {
-      return await axios.get(`http://localhost:3000/join-room?identity?=${this.username}`);
+      return await axios.get(`http://localhost:3000/token?identity=${this.username}`);
     },
     // Trigger log events 
    dispatchLog(message) {
